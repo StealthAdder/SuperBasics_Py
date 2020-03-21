80abbd7a853733a14a38124e8a9a49e5c5cb8fdf
Hariharan Parthiban <59941004+StealthAdder@users.noreply.github.com>
2020-03-21 23:48:06 +0530

    Tuples in python
    Examples for Tuple

---
+++ tuples.py
@@ -0,0 +1,20 @@
+# tuples
+
+# tuples is type of data structure
+# its similar to a container or list, where we can store different values
+
+# create a tuple
+# coordinates 
+#tuples are immutable (can't modified)
+
+# single tuple
+coordinates = (4, 5)
+print(coordinates[1]) 
+# access using index
+# OUTPUT: 5
+# ---------------------------------------------------------------------------------------------
+coordinates = [(4, 5), (25, 15), (30, 55), (45, 22)] #list of coordinates for tuple
+print(coordinates)
+
+# OUTPUT: [(4, 5), (25, 15), (30, 55), (45, 22)]
+# ------------------------------------------------------------------------------------------------
